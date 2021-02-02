465baba9e03f692d9bfd3db946faea985ce3d687
sadewo1004 <63345660+sadewo1004@users.noreply.github.com>
2021-02-02 11:38:31 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong>MS Softwere House </strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Pengajuan Rumah Tidak Layak Huni</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logodinsos.jpg" width="500px"></img></p>
+                          <p class="label-title" align="center"><strong>DINAS SOSIAL KABUPATEN ASAHAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Pengajuan RTLH Rumah Tidak Layak Huni Dinas Sosial.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data penduduk, pengelolaan pengajuan RTLH, <br>
+                            dan juga pengelolaan laporan penduduk serta pengajuannya.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data penduduk dan data pengajuan.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
